85d363996af2dcca2fa98990a2dcf4279a9f835c
ElizabethKost <106992754+ElizabethKost@users.noreply.github.com>
2023-12-04 13:18:11 +0200

    Add files via upload

---
+++ lesson_18/js/script.js
@@ -0,0 +1,15 @@
+const icon = document.querySelector('.icon-menu');
+icon.addEventListener('click', function () {
+	document.documentElement.classList.toggle('menu-open');
+});
+
+const icon2 = document.querySelector('.icon2-menu');
+icon2.addEventListener('click', function () {
+	document.documentElement.classList.toggle('menu-open2');
+});
+
+const icon3 = document.querySelector('.icon3-menu');
+icon3.addEventListener('click', function () {
+	document.documentElement.classList.toggle('menu-open3');
+});
+
